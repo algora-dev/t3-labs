383a2d22e0a4c4e9e11ec95c7938276f8f16ed95
algora-dev <268036249+algora-dev@users.noreply.github.com>
2026-07-13 21:29:02 +0100

    Add reusable website template proposal

--- sites/index.ts
+++ sites/index.ts
@@ -1,7 +1,11 @@
 import { falconContracting } from "./falcon-contracting-wp-000205/site-config";
+import { t3LabsWebsiteTemplate } from "./t3labs-website-template-1993/site-config";
 import type { ProspectSiteConfig } from "./types";
 
-export const sites = { [falconContracting.slug]: falconContracting } satisfies Record<string, ProspectSiteConfig>;
+export const sites = {
+  [falconContracting.slug]: falconContracting,
+  [t3LabsWebsiteTemplate.slug]: t3LabsWebsiteTemplate,
+} satisfies Record<string, ProspectSiteConfig>;
 export type SiteSlug = keyof typeof sites;
 export const siteSlugs = Object.keys(sites) as SiteSlug[];
 export function getSite(slug: string): ProspectSiteConfig | undefined { return sites[slug as SiteSlug]; }
