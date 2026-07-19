e8d3e75b9a54be2573a4958b7c3dbe8ea99615ad
algora-dev <268036249+algora-dev@users.noreply.github.com>
2026-07-19 22:58:54 +0100

    Add 3A Roofing proposal page

--- sites/index.ts
+++ sites/index.ts
@@ -2,12 +2,14 @@ import { falconContractingProposal } from "./falcon-contracting-wp-000205/propos
 import { aspireMembranesProposal } from "./aspire-membranes-wp-000206/proposal-config";
 import { atkinsonBuildingServicesProposal } from "./atkinson-building-services-wp-000208/proposal-config";
 import { proposalTemplate } from "./proposal-template-wp-000000/proposal-config";
+import { threeARoofingProposal } from "./3a-roofing-wp-000207/proposal-config";
 import type { ProposalConfig } from "./types";
 
 export const sites = {
   [falconContractingProposal.slug]: falconContractingProposal,
   [aspireMembranesProposal.slug]: aspireMembranesProposal,
   [atkinsonBuildingServicesProposal.slug]: atkinsonBuildingServicesProposal,
+  [threeARoofingProposal.slug]: threeARoofingProposal,
   [proposalTemplate.slug]: proposalTemplate,
 } satisfies Record<string, ProposalConfig>;
 
